d6353382e20490bf5986e39c8c3ac0a0ca9b49b1
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-08 10:53:51 +0530

    app-module

--- main.tf
+++ main.tf
@@ -115,6 +115,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo labauto ansible",
+      "sudo set-hostname -skip-apply ${var.component}",
       "ansible-pull -i localhost, -U https://github.com/siva-devops73/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component}",
     ]
   }
