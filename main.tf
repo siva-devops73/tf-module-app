ef11def99a0a6ef8a8b19691e56ecec05eb602fb
Siva Kumar <sivakumar9399959803@gmail.com>
2023-08-21 10:07:58 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -30,6 +30,15 @@ resource "aws_security_group" "main" {
   }
 }
 
+## Target group
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+
 ## Launch Template of Auto Scaling Group
 resource "aws_launch_template" "main" {
   name = "${var.component}-${var.env}"
@@ -69,6 +78,7 @@ resource "aws_autoscaling_group" "main" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns   = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.main.id
